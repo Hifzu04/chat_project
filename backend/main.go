54f9616e8372d2b1fdf045e8f7a45e0426784639
Hifzu04 <hifzurbtech@gmail.com>
2025-06-07 11:16:16 +0530

    connected to the mongodb server

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	config "chat-backend/Config"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("welcome to backend")
+
+	//load from env
+	if err := godotenv.Load(); err != nil {
+		log.Println("no env found")
+	}
+
+	//connect to mongodb , read mongodb uri
+	config.ConnectDB()
+
+	//router
+	///router := routes.RegisterRoutes()
+
+	//port
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+	fmt.Printf("server is listening on : %s\n", port)
+	
+	//log.Fatal(http.ListenAndServe(":"+port, router))
 }
