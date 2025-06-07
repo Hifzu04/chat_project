161c6120d229530f74ac3c1ced7b41de6d9aaac2
Hifzu04 <hifzurbtech@gmail.com>
2025-06-07 11:16:16 +0530

    connected to the mongodb server

---
+++ backend/Config/db.go
@@ -0,0 +1,50 @@
+//config database to read mongodb uri
+
+package config
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	DBClient *mongo.Client
+	DBName   = os.Getenv("chatdb")
+)
+
+func ConnectDB() {
+	uri := os.Getenv("MONGODB_URI")
+	if uri == "" {
+		log.Fatal("mongodb uri is not set in the environment")
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+
+	defer cancel()
+
+	clientOpts := options.Client().ApplyURI(uri)
+
+	client, err := mongo.Connect(ctx, clientOpts)
+
+	if err != nil {
+		log.Fatalf("Mongo connect %v", err)
+	}
+
+	if err := client.Ping(ctx, nil); err != nil {
+		log.Fatalf("Mongo ping %v", err)
+	}
+	fmt.Println("Yeaa, connected to mongodb")
+
+	DBClient = client
+}
+
+func GetCollection(name string) *mongo.Collection {
+	db := DBClient.Database(DBName)
+	return db.Collection("users")
+}
